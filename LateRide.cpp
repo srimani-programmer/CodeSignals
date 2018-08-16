8017777aa67f012a23b263b1da4296ff094e9fd3
srimani-programmer <srimani.scholar@gmail.com>
2018-08-16 18:38:57 +0530

    Solution

--- LateRide.cpp
+++ LateRide.cpp
@@ -3,25 +3,19 @@
 using namespace std;
 
 int lateRide(int n) {
-    int sum1 = 0;
-    int sum2 = 0;
-    int res1 = n/60;
-    double res2 = (double)n/(double)60;
-    int min = (res2 - res1) * 60;
-    int rem1 = 0,rem2 = 0;
-    while(res1 != 0){
-        rem1 = res1%10;
-        sum1 = sum1 + rem1;
-        res1 = res1/10;
-    }
-    while(min != 0){
-        rem2 = min%10;
-        sum2 = sum2 + rem2;
-        min = min/10;
-    }
-
-    return sum1+sum2;
+    int sum1=0;
+    int sum2=0;
+    int hour=n/60;
+    int minute=n%60;
+    do{
+        sum1+=hour%10;
+        hour=hour/10;
+        sum2+=minute%10;
+        minute=minute/10;
+    } while(hour>0 || minute>0);
+return sum1+sum2;
 }
+
 
 int main(){
     int val;
