c0751e45cfa41607d89a49476b37fc2a4748afd8
srimani-programmer <srimani.scholar@gmail.com>
2018-11-06 10:18:27 +0530

    Solutions for the company challenges.

---
+++ Company Challenges/UBER/FairEstimator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+std::vector<double> fareEstimator(int ride_time, int ride_distance, std::vector<double> cost_per_minute, std::vector<double> cost_per_mile) {
+    std::vector<double> res;
+    double res1;
+    for(int i=0;i<cost_per_mile.size();i++){
+        res1 = 0.0;
+        res1 = res1 + ((ride_time * cost_per_minute[i]) + (ride_distance * cost_per_mile[i]));
+        
+        res.push_back(res1);
+    }
+      
+    return res;  
+}
